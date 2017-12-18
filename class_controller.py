5901b33f31e50b2f8d0ea9fd8ccbf014bec85818
Niels Heissel <niels.heissel@konzepthaus-ws.de>
2017-12-18 17:36:41 +0100

    inserted list

--- class_controller.py
+++ class_controller.py
@@ -12,6 +12,7 @@ from tkinter import messagebox
 import sys
 import os
 import time
+import math
 
 
 __author__ = "6785468: Robert am Wege, 6770541: Niels Heissel"
@@ -41,21 +42,28 @@ class SnakeGame():
         self.lw.pack()
 
         self.direction = 1
+        self.food_coords = []
+        self.snake_body = []
+        self.snake_coords = []
 
-        self.circ = self.lw.create_oval(0, 0, 80, 80, fill="green")
+        self.circ = self.lw.create_oval(0, 0, 20, 20, fill="green")
+        self.snake_body.append(self.circ)
         self.x1 = 0
         self.y1 = 0
         self.speed = 2
-        self.root.after(1000, self.animation)
+        self.root.after(1000, self.snake_coords_inc)
+        self.root.after(1000, self.update_snake_pos)
 
         self.root.bind('<Down>', self.change_diretion_down)
         self.root.bind('<Right>', self.change_diretion_right)
         self.root.bind('<Left>', self.change_diretion_left)
         self.root.bind('<Up>', self.change_diretion_up)
 
+        self.root.after(3000, self.food_generator)
+
         self.root.mainloop()
 
-    def animation(self):
+    def snake_coords_inc(self):
         self.check_collisions()
         if self.direction == 1:
             self.x1 += self.speed
@@ -65,9 +73,17 @@ class SnakeGame():
             self.x1 -= self.speed
         elif self.direction == 4:
             self.y1 -= self.speed
-        print("heyyy", self.x1)
-        self.lw.coords(self.circ, self.x1, self.y1, self.x1 + 20, self.y1 + 20)
-        self.root.after(10, self.animation)
+        self.snake_coords.insert(0, (self.x1, self.y1))
+        self.root.after(10, self.snake_coords_inc)
+
+    def update_snake_pos(self):
+        m = 0
+        for part in self.snake_body:
+            x = self.snake_coords[m][0]
+            y = self.snake_coords[m][1]
+            self.lw.coords(part, x, y, x + 20, y + 20)
+            m += 1
+        self.root.after(10, self.update_snake_pos)
 
     def check_collisions(self):
         if self.x1 > 380:
@@ -78,6 +94,10 @@ class SnakeGame():
             sys.exit()
         if self.y1 < 0:
             sys.exit()
+        for food in self.food_coords:
+            if math.isclose(self.x1, food[0]):
+                print("HIT")
+                self.grow()
 
     def change_diretion_down(self, event):
         self.direction = 2
@@ -91,6 +111,20 @@ class SnakeGame():
     def change_diretion_left(self, event):
         self.direction = 3
 
+    def grow(self):
+        print(self.x1 - 20, self.y1 - 20, self.x1, self.y1, self.x1)
+        m = self.lw.create_oval(10, 10, 30, 30, fill="red")
+        self.lw.tag_lower(m)  # lowers the level of element
+        self.snake_body.append(m)
+
+    def food_generator(self):
+        x = random.randint(0, 380)
+        y = random.randint(0, 280)
+        self.food = self.lw.create_oval(x, y, x+20, y+20, fill="red")
+        self.lw.tag_lower(self.food)  # lowers the level of element
+        self.root.after(5000, self.food_generator)
+        print(self.lw.coords(self.food)[0:2])
+        self.food_coords.append(self.lw.coords(self.food)[0:2])
 
     def __del__(self):
         print("Instance deleted.")
