2f06f63d1f57dea45f93934b66dfd1499dc62b02
Niels Heissel <niels.heissel@konzepthaus-ws.de>
2017-12-21 14:51:51 +0100

    Niels Snake

--- scr/snake/class_controller.py
+++ scr/snake/class_controller.py
@@ -29,9 +29,10 @@ class SnakeGame():
         self.root = tk.Tk()
         self.root.attributes("-topmost", True)  # put the root to foreground
         self.root.geometry('+900+50')  # sets the default window position
-        self.root.geometry('500x320')
+        self.root.geometry('405x340')
         self.root.title("Snake")
 
+        self.game_state = False
         self.speed = 1
         self.direction = 1
         self.food_coords = []
@@ -48,34 +49,62 @@ class SnakeGame():
         self.speed_var.set("Speed")
 
         self.lw = tk.Canvas(bg="black", width=400, height=300)
-        self.lw.grid(row=0, column=0, rowspan=6, columnspan=9)
+        self.lw.grid(rowspan=3, columnspan=3)
 
-        self.label_1 = tk.Label(self.root, textvariable=self.score_var, bg="red")
-        self.label_1.grid(row=3, column=10)
+        self.label_1 = tk.Label(self.root, textvariable=self.score_var, bg="red", relief=tk.RIDGE, bd="5", width=10,
+                                height=1)
+        self.label_1.grid(row=3, column=0)
 
-        self.label_2 = tk.Label(self.root, textvariable=self.speed_var, bg="green")
-        self.label_2.grid(row=4, column=10)
+        self.start_button = tk.Button(self.root, text="Start", command=self.start)
+        self.start_button.grid(row=3, column=1)
+
+        self.label_2 = tk.Label(self.root, textvariable=self.speed_var, bg="blue", relief=tk.RIDGE, bd="5", width=10,
+                                height=1)
+        self.label_2.grid(row=3, column=2)
 
         self.circ = self.lw.create_oval(0, 0, 20, 20, fill="green")
         self.snake_body.append(self.circ)
         self.x1 = 0
         self.y1 = 0
 
-        messagebox.showwarning("Instructions", "Normal Snake Game\nPlease use the arrow keys to navigate the snake.")
-
         self.root.bind('<Down>', self.change_diretion_down)
         self.root.bind('<Right>', self.change_diretion_right)
         self.root.bind('<Left>', self.change_diretion_left)
         self.root.bind('<Up>', self.change_diretion_up)
 
-        self.root.after(1000, self.snake_coords_inc)
-        self.root.after(1000, self.update_snake_pos)
+        self.instructions()
+
+        self.root.mainloop()
+
+        sys.exit()
+
+    def start(self):
+        self.delete_instructions()
+        self.game_state = True
+        self.snake_coords_inc()
+        self.update_snake_pos()
         self.root.after(3000, self.food_generator)
         self.root.after(4000, self.inc_speed)
         self.root.after(5000, self.block_generator)
         self.root.after(30000, self.block_destructor)
+        self.start_button.destroy()
 
-        self.root.mainloop()
+
+    def instructions(self):
+        self.text = self.lw.create_text(200, 115, text="Welcome to Snake. \nThe original game "
+                                                      "with some small changes. \nTo navigate "
+                                                      "with the snake, use the arrow keys\n"
+                                                      "(<up>, <down>, <right>, <left>). \n"
+                                                      "But don't turn to quickly or you will bite"
+                                                      " yourself...\n"
+                                                      "Be careful, your speed increases every "
+                                                      "10 seconds. \n\nEat the red ovals to score, "
+                                                      "hit the grey to slow down, but \nwatch out,"
+                                                      " you might get torn apart.",
+                                        fill="white", font=("Purisa", 15))
+
+    def delete_instructions(self):
+        self.lw.delete(self.text)
 
     def snake_coords_inc(self):
         self.check_collisions()
@@ -97,46 +126,64 @@ class SnakeGame():
             y = self.snake_coords[m][1]
             self.lw.coords(part, x, y, x + 20, y + 20)
             m += self.steps_per_body
-        self.score_var.set(self.score)
         self.speed_var.set(self.speed)
         self.root.after(1, self.update_snake_pos)
 
     def check_collisions(self):
-        if self.x1 > 380:
-            messagebox.showwarning("Game Over", "Game Over! \nYou went out of the field.")
-            sys.exit()
-        if self.y1 > 280:
-            messagebox.showwarning("Game Over", "Game Over! \nYou went out of the field.")
-            sys.exit()
-        if self.x1 < 0:
-            messagebox.showwarning("Game Over", "Game Over! \nYou went out of the field.")
-            sys.exit()
-        if self.y1 < 0:
-            messagebox.showwarning("Game Over", "Game Over! \nYou went out of the field.")
-            sys.exit()
-        for food in self.foods:
-            if self.x1 == self.lw.coords(food)[0] and self.y1 == self.lw.coords(food)[1]:
-                print("HIT")
-                self.grow()
-                self.lw.delete(food)
-                self.foods.remove(food)
-                self.score += 1
-                print(self.score)
+        if self.game_state == True:
+            if self.x1 > 380:
+                self.game_state = False
+                self.lw.delete("all")
+                self.lw.create_text(200,100, text="GAME OVER \nYou lost...", fill="grey", font=("Purisa", 30))
+                return
+            if self.y1 > 280:
+                self.game_state = False
+                self.lw.delete("all")
+                self.lw.create_text(200,100, text="GAME OVER \nYou lost...", fill="grey", font=("Purisa", 30))
+                return
+            if self.x1 < 0:
+                self.game_state = False
+                self.lw.delete("all")
+                self.lw.create_text(200,100, text="GAME OVER \nYou lost...", fill="grey", font=("Purisa", 30))
+                return
+            if self.y1 < 0:
+                self.game_state = False
+                self.lw.delete("all")
+                self.lw.create_text(200,100, text="GAME OVER \nYou lost...", fill="grey", font=("Purisa", 30))
+                return
+            for food in self.foods:
+                if self.x1 == self.lw.coords(food)[0] and self.y1 == self.lw.coords(food)[1]:
+                    self.grow()
+                    self.lw.delete(food)
+                    self.foods.remove(food)
+                    self.score += 1
+                    self.score_var.set(self.score)
 
-        for part in self.snake_coords[1:len(self.snake_body) * self.steps_per_body]:
-            if part[0] == self.x1 and part[1] == self.y1:
-                messagebox.showwarning("Game Over", "Game Over! \nYou ate yourself.")
-                sys.exit()
+            for part in self.snake_coords[1:len(self.snake_body) * self.steps_per_body]:
+                if part[0] == self.x1 and part[1] == self.y1:
+                    self.game_state = False
+                    self.lw.delete("all")
+                    self.lw.create_text(200,100, text="GAME OVER. You lost...", fill="grey", font=("Purisa", 30))
+                    return
 
-        for block in self.blocks:
-            try:
-                if self.x1 == self.lw.coords(block)[0] and self.y1 == self.lw.coords(block)[1]:
-                    self.dec_speed()
-            except IndexError:
-                None
+            for block in self.blocks:
+                try:
+                    if self.x1 == self.lw.coords(block)[0] and self.y1 == self.lw.coords(block)[1]:
+                        self.label_3 = tk.Label(self.root, text="Watch out for obstacles..", bg="grey")
+                        self.label_3.grid(row=3, column=1)
+                        self.root.after(2000, self.delete_message)
+                        self.dec_speed()
+                except IndexError:
+                    None
+
+    def delete_message(self):
+        try:
+            self.label_3.destroy()
+        except AttributeError:
+            None
 
     def change_diretion_down(self, event):
-        if self.key_pressed is False:
+        if self.key_pressed is False and self.game_state is True:
             if self.direction == 4:
                 return
             if self.snake_coords[0][0] % 20 == 0 and self.snake_coords[0][1] % 20 == 0:
@@ -155,7 +202,7 @@ class SnakeGame():
             self.root.after(10, self.keep_change_down)
 
     def change_diretion_right(self, event):
-        if self.key_pressed is False:
+        if self.key_pressed is False and self.game_state is True:
             if self.direction == 3:
                 return
             if self.snake_coords[0][0] % 20 == 0 and self.snake_coords[0][1] % 20 == 0:
@@ -174,7 +221,7 @@ class SnakeGame():
             self.root.after(10, self.keep_change_right)
 
     def change_diretion_up(self, event):
-        if self.key_pressed is False:
+        if self.key_pressed is False and self.game_state is True:
             if self.direction == 2:
                 return
             if self.snake_coords[0][0] % 20 == 0 and self.snake_coords[0][1] % 20 == 0:
@@ -194,7 +241,7 @@ class SnakeGame():
             self.root.after(10, self.keep_change_up)
 
     def change_diretion_left(self, event):
-        if self.key_pressed is False:
+        if self.key_pressed is False and self.game_state is True:
             if self.direction == 1:
                 return
             if self.snake_coords[0][0] % 20 == 0 and self.snake_coords[0][1] % 20 == 0:
@@ -222,7 +269,7 @@ class SnakeGame():
         y = self.snake_coords[l * self.steps_per_body - 1][1]
         print(x)
         print(self.x1)
-        m = self.lw.create_oval(x, y, x + 20, y + 20, fill="green")
+        m = self.lw.create_oval(x, y, x + 20, y + 20, fill="darkgreen")
         self.snake_body.append(m)
 
     def food_generator(self):
@@ -237,12 +284,12 @@ class SnakeGame():
                 continue
             else:
                 break
-        self.food = self.lw.create_oval(x, y, x + 20, y + 20, fill="red")
-        self.lw.tag_lower(self.food)  # lowers the level of element
-        self.root.after(5000, self.food_generator)
-        print(self.lw.coords(self.food)[0:2])
-        self.food_coords.append(self.lw.coords(self.food)[0:2])
-        self.foods.append(self.food)
+        food = self.lw.create_oval(x, y, x + 20, y + 20, fill="red")
+        self.lw.tag_lower(food)  # lowers the level of element
+        if self.game_state is True:
+            self.root.after(5000, self.food_generator)
+        self.food_coords.append(self.lw.coords(food)[0:2])
+        self.foods.append(food)
 
     def block_generator(self):
         """Randomly positions blocks on the field."""
@@ -256,10 +303,11 @@ class SnakeGame():
                 continue
             else:
                 break
-        self.block = self.lw.create_oval(x, y, x + 20, y + 20, fill="grey")
-        self.lw.tag_lower(self.block)  # lowers the level of element
-        self.root.after(5000, self.block_generator)
-        self.blocks.append(self.block)
+        block = self.lw.create_oval(x, y, x + 20, y + 20, fill="grey")
+        self.lw.tag_lower(block)  # lowers the level of element
+        if self.game_state is True:
+            self.root.after(5000, self.block_generator)
+        self.blocks.append(block)
 
     def block_destructor(self):
         """Randomly positions blocks on the field."""
@@ -279,8 +327,8 @@ class SnakeGame():
             self.root.after(5, self.inc_speed)
             return
         self.steps_per_body = int(20 / self.speed)
-
-        self.root.after(10000, self.inc_speed)
+        if self.game_state is True:
+            self.root.after(10000, self.inc_speed)
 
     def dec_speed(self):
         if self.x1 % 20 == 0 and self.y1 % 20 == 0:
